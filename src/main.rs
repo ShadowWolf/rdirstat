74341c536b710140d5aaf03c01ccc80144be78b0
Bryan Wolf <shadowwolf007@gmail.com>
2022-09-15 15:22:11 -0500

    Remove some minor comments

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,6 @@ fn main() {
 
     let item_type = ItemType::process_path("C:\\Program Files");
     item_type.print(2, 0);
-    // match item_type {
-    //     ItemType::Directory { size, path, children } => {
-    //         println!("total size of {path}: {size}")
-    //     }
-    //     ItemType::File { size, path, .. } => {
-    //         println!("total size of {path}: {size}")
-    //     }
-    // }
-
     let elapsed = now.elapsed();
     println!("{:.2?}", elapsed);
 
